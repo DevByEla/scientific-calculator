0d32374cc3dd98ea5b46ba8f366b070afc6d9e02
DevByEla <gabrielanitu34@yahoo.com>
2024-10-01 20:58:20 +0000

    Added some advanced functionalities

--- src/script.js
+++ src/script.js
@@ -10,7 +10,15 @@ document.addEventListener("DOMContentLoaded", function(){
         const convertedValue = currentValue
             .replace("×", "*")
             .replace("÷", "/")
-            .replace("%", "/100");
+            .replace("%", "/100")
+            .replace("sin", 'Math.sin')
+            .replace("cos", 'Math.cos')
+            .replace("ln", 'Math.log')
+            .replace("π", 'Math.PI')
+            .replace("log", 'Math.log10')
+            .replace('e', 'Math.E')
+            .replace("tan", 'Math.tan')
+            .replace("√", 'Math.sqrt');
         console.log('converted value: ', convertedValue);
         const result = eval(convertedValue);
         currentValue = result.toString(); 
@@ -22,7 +30,8 @@ document.addEventListener("DOMContentLoaded", function(){
         button.addEventListener('click', function(){
             const value = button.innerText;
             
-            if(value == "AC"){
+            try{
+              if(value == "AC"){
                 currentValue = "";
                 display.value = currentValue;
             }else if(value == "="){
@@ -32,7 +41,14 @@ document.addEventListener("DOMContentLoaded", function(){
                 currentValue += value;
                 console.log('current value: ', currentValue);
                 display.value = currentValue;
+            }  
+            }catch (error){
+                console.error(error);
+                currentValue = "ERROR";
+                display.value = currentValue;
             }
+
+            
             
         })
     }
